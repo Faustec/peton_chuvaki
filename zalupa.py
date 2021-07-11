aa6659c2abc8ead8e275c7aef350f43b47855754
Faustec <emhil@mail.ru>
2021-07-11 14:18:58 +0500

    залупа сломалась

--- zalupa.py
+++ zalupa.py
@@ -1,12 +1,13 @@
 def NewFunction(sm):
- a = sm
- if a
-    print("Моя залупа ", sm, " сантиметров")
-
-
-
-
-
-
-    
-NewFunction(23)
+ a = sm % 10
+ if a == 1 and sm != 11:
+    print("Моя залупа ", sm, " сантиметр")
+ elif a > 1 and a < 5 and sm < 20:
+    print("Моя залупа ", sm, " сантиметра")
+ else: 
+    print("Моя залупа ", sm, " сантиметров")   
+ 
+i = 1
+while i < 90:
+   NewFunction(i)
+   i = i + 1
