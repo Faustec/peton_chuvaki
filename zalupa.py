f55e8c8de0c037ea04379dca0cdfb26a0c631834
Faustec <emhil@mail.ru>
2021-07-11 18:06:43 +0500

    Сделяль

--- zalupa.py
+++ zalupa.py
@@ -2,7 +2,7 @@ def NewFunction(sm):
  a = sm % 10
  if a == 1 and sm != 11:
     print("Моя залупа ", sm, " сантиметр")
- elif a > 1 and a < 5 and sm < 20:
+ elif a > 1 and a < 5 and (sm < 10 or sm > 20):
     print("Моя залупа ", sm, " сантиметра")
  else: 
     print("Моя залупа ", sm, " сантиметров")   
